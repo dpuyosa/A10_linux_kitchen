b46e581d5e1b6dfad255bebfaf41fb82a5c5bf24
dpuyosa <dpuyosa@users.noreply.github.com>
2015-02-20 16:49:27 +0100

    Bugfix

--- scripts/build_root.sh
+++ scripts/build_root.sh
@@ -7,7 +7,7 @@ if [ -e ./_temp/root/cmd_line.cfg ]
 then
 cmd_line=`cat ./_temp/root/cmd_line.cfg`
 ./tools/mkbootfs ./_temp/root/ramdisk | gzip > ./_temp/root/ramdisk-new.gz
-./tools/mkbootimg --base 0x40008000 --kernel ./_temp/root/root.fex-kernel --ramdisk ./_temp/root/ramdisk-new.gz --cmdline "$cmd_line" -o ./_temp/root/new-root.fex
+./tools/mkbootimg --base 0x40000000 --kernel ./_temp/root/root.fex-kernel --ramdisk ./_temp/root/ramdisk-new.gz --cmdline "$cmd_line" -o ./_temp/root/new-root.fex
 rm ./_temp/root/ramdisk-new.gz
 mv ./_temp/root/new-root.fex ./_input/root.fex
 echo "Done..."
